0cd3f4be655a5bc574b628e62948e8fd4b420c44
Justin Huynh <justinhuynh639@gmail.com>
2023-10-01 21:34:23 -0700

    formatted random dice rolls

--- app.py
+++ app.py
@@ -4,4 +4,14 @@ import random
 #define a function to roll the dice
 def roll_dice():
 
+    roll = input("Roll the dice? (Yes/No): ")
+
+    #create while loop that makes input lowercase
+    while roll.lower() == "Yes".lower():
+        #assign random integer between 2 inputted numbers
+        dice1 = random.randint(1, 6)
+        dice2 = random.randint(1, 6)
+
+        #print a statement that has a dictionary formatted for dice1 and dice2
+        print("dice rolled: {} and {}".format(dice1, dice2))
 #create a dictionary that will have the drwaings of the dice
